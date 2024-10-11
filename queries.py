ec15217db242656587599e5342d00a61318ebe98
Mario Abarca <mario.abarca@magna.com>
2024-10-11 12:36:17 -0600

    Improve queries.py

--- queries.py
+++ queries.py
@@ -53,48 +53,51 @@ class ProcessQueries:
         }
         turn = support.Support().get_turn()
 
-        data_results = []
         data_results = self.query.get_cost_wight_hour()
 
-         # Convertir las horas de los registros a offset-naive
-        data_results = [(item[0], item[1], item[2].replace(tzinfo=None)) for item in data_results]
+        # Convertir las horas de los registros a offset-naive
+        data_results = [
+                (
+                    item[0],
+                    item[1],
+                    item[2].replace(tzinfo=None)
+                )for item in data_results
+            ]
 
         cost = sum(item[0] for item in data_results)
         weight = sum(item[1] for item in data_results)
 
         if turn == 1:
-            cost_turn = sum(item[0] for item in data_results
-                            if item[2] >= turns_init[1]
-                            and item[2] < turns_init[2])
-
-            weight_turn = sum(item[1] for item in data_results
-                              if item[2] >= turns_init[1]
-                              and item[2] < turns_init[2])
-
+            cost_turn = sum(item[0] for item
+                            in data_results
+                            if turns_init[1] <= item[2] < turns_init[2]
+                        )
+            weight_turn = sum(item[1] for item
+                              in data_results
+                              if turns_init[1] <= item[2] < turns_init[2]
+                        )
         elif turn == 2:
-            cost_turn = sum(item[0] for item in data_results
-                            if item[2] >= turns_init[2]
-                            and item[2] < turns_init[3])
-
-            weight_turn = sum(item[1] for item in data_results
-                              if item[2] >= turns_init[2]
-                              and item[2] < turns_init[3])
-
+            cost_turn = sum(item[0] for item
+                            in data_results 
+                            if turns_init[2] <= item[2] < turns_init[3]
+                        )
+            weight_turn = sum(item[1] for item
+                              in data_results
+                              if turns_init[2] <= item[2] < turns_init[3]
+                        )
         else:
-            cost_turn = sum(item[0] for item in data_results
+            cost_turn = sum(item[0] for item
+                            in data_results
                             if item[2] >= turns_init[3]
-                            or item[2] < turns_init[1])
-
-            weight_turn = sum(item[1] for item in data_results
+                            or item[2] < turns_init[1]
+                        )
+            weight_turn = sum(item[1] for item
+                              in data_results 
                               if item[2] >= turns_init[3]
-                              or item[2] < turns_init[1])
+                              or item[2] < turns_init[1]
+                        )
 
-        cost = round(cost, 2)
-        weight = round(weight, 2)
-        cost_turn = round(cost_turn, 2)
-        weight_turn = round(weight_turn, 2)
-
-        return cost, weight, cost_turn, weight_turn
+        return round(cost, 2), round(weight, 2), round(cost_turn, 2), round(weight_turn, 2)
 
 if __name__ == "__main__":
     process_queries = ProcessQueries()
